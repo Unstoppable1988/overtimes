8e0b3bbe36c1ddcfa1cae7aa18ca38b6891da530
Sergey Yerofeyev <termo1988@gmail.com>
2022-01-22 19:47:18 +0200

    Release v_1.0

--- src/components/OvertimesTable/OvertimesTable.js
+++ src/components/OvertimesTable/OvertimesTable.js
@@ -1,6 +1,6 @@
 import { useHttp } from "../../hooks/http.hook";
 import { useState, useEffect } from "react";
-import { Container, Row, Col } from "react-bootstrap";
+import { Container, Row, Col, Button } from "react-bootstrap";
 import AddOvertimeForm from "../AddOvertimeForm/AddOvertimeForm";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
@@ -10,18 +10,19 @@ const OvertimesTable = () => {
     const {request} = useHttp();
 
     useEffect(() => {
-        getOvertimes();
+        getOvertimes(false);
     }, []);
 
-    const getOvertimes = () => {
+    const getOvertimes = (paidStatus) => {
         request(`http://localhost:3001/overtimes/`)
-        .then(data => setOvertimes(data.filter(item => item.paid === false)))
+        .then(data => setOvertimes(data.filter(item => item.paid === paidStatus)))
         .catch(err => console.log(err));
     };
 
-    const onPaid = (id) => {
+    const onPaidSwitch = (id, status) => {
         let checkedItem = overtimes.filter(item => item.id === id);
-        checkedItem = {...checkedItem[0], paid: true}
+        checkedItem = {...checkedItem[0], paid: status}
+        console.log(checkedItem);
         request(`http://localhost:3001/overtimes/${id}`, "PUT", JSON.stringify(checkedItem))
         .then(setOvertimes(overtimes.filter(item => item.id !== id)))
         .catch(err => console.log(err));
@@ -67,15 +68,17 @@ const OvertimesTable = () => {
     const renderOvertimes = (overtimes) => {
         const items = overtimes.map(item => {
             return (
+                <>
                 <Row className="mt-2" key={item.id}>
                     <Col>{item.number}</Col>
                     <Col>{item.start}</Col>
                     <Col>{item.end}</Col>
                     <Col>{item.overtime}</Col>
                     <Col className="col-4">{item.note}</Col>
-                    <Col><button className="btn btn-primary" onClick={() => onPaid(item.id)}>Paid</button></Col>
-                    <Col><button className="btn btn-primary" onClick={() => onDelete(item.id)}>Delete</button></Col>
+                    <Col><Button className="btn btn-primary" onClick={() => onPaidSwitch(item.id, item.paid ? false : true)}>{item.paid ? 'Unpaid' : 'Paid'}</Button></Col>
+                    <Col><Button className="btn btn-primary" onClick={() => onDelete(item.id)}>Delete</Button></Col>
                 </Row>
+                </>
             )
         });
         return (
@@ -85,13 +88,31 @@ const OvertimesTable = () => {
         )
     }
 
+    const SwitchBtn = () => {
+        const status = overtimes.length === 0 ? 'unpaid' : overtimes[0].paid ? 'unpaid' : 'paid';
+        const boolStatus = overtimes.length === 0 ? false : overtimes[0].paid ? false : true;
+        return (
+            <Button className="btn btn-info" onClick={() => getOvertimes(boolStatus)}>Show {status}</Button>
+        )
+    }
+
     const items = renderOvertimes(overtimes);
 
-    return (
+    return (        
         <Container>
             <Row>
                 <Col className="col-8">
+                    <Row className="mt-2" style={{"fontWeight":"bold"}}>
+                        <Col>Number</Col>
+                        <Col>Start</Col>
+                        <Col>End</Col>
+                        <Col>Overtime</Col>
+                        <Col className="col-4">Note</Col>
+                        <Col></Col>
+                        <Col></Col>
+                    </Row>
                     {items}
+                    <SwitchBtn/>
                 </Col>
                 <Col className="col-4">
                     <AddOvertimeForm onAdd={onAdd}/>
